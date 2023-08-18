fb42405ca55665a84e5186d7eda96aa4413dbf78
gabriellelailler <gabrielle.lailler@gmail.com>
2023-08-18 15:30:48 +0200

    suppression user.js

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -5,9 +5,9 @@ const bookCtrl = require('../controllers/book');
 
 router.get('/', bookCtrl.getAllBook);
 router.post('/', bookCtrl.createBook);
+router.get('/bestrating', bookCtrl.getThreeBook);
 router.get('/:id', bookCtrl.getOneBook);
 router.put('/:id', bookCtrl.modifyBook);
 router.delete('/:id', bookCtrl.deleteBook);
-router.get('/bestrating', bookCtrl.getThreeBook);
 
 module.exports = router;
