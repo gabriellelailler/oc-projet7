93592e3f1001b1337f7e65a59a4ace787afa1e4c
gabriellelailler <gabrielle.lailler@gmail.com>
2023-08-18 11:50:06 +0200

    toutes routes sans spécifications

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,15 @@ app.get('/api/books', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
   });
 
+app.post('/api/books', (req, res, next) => {
+    delete req.body._id;
+    const book = new Book({
+      ...req.body
+    });
+    book.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
 app.get('/api/books/:id', (req, res, next) => {
     Book.findOne({ _id: req.params.id })
@@ -40,13 +49,15 @@ app.get('/api/books/:id', (req, res, next) => {
         .catch(error => res.status(404).json({ error }));
   });
 
-app.post('/api/books', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Book({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+app.put('/api/books/:id', (req, res, next) => {
+    Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+    Book.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
       .catch(error => res.status(400).json({ error }));
   });
 
